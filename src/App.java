f7a52de7c2e6a54234afb694d484ad6554bf1cfd
Marcos20005 <mo205016@gmail.com>
2025-04-29 17:37:32 -0700

    new file:   .vscode/settings.json new file:   META-INF/MANIFEST.MF new file:   README.md new file:   bin/App.class new file:   src/App.java

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+      JFrame cuadrado = new JFrame();
+        cuadrado.setTitle("Aplicacion hecha por Marcos");
+        cuadrado.setSize(500, 500);
+        cuadrado.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        cuadrado.setLocationRelativeTo(null);   
+        cuadrado.setVisible(true);
+        
+
+    }
+}
